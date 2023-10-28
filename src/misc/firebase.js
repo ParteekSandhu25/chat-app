2549c7d7f407271752a0d421dd6cc59aa195be72
Parteek Sandhu <parteek.sandhu@s.amity.edu>
2023-10-28 11:47:47 +0530

    initialized firebase to project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDlYo9prCcvGACS2lCr0gzK8TVPwlN1qdw',
+  authDomain: 'chat-web-app-e80fb.firebaseapp.com',
+  projectId: 'chat-web-app-e80fb',
+  storageBucket: 'chat-web-app-e80fb.appspot.com',
+  messagingSenderId: '764629843466',
+  appId: '1:764629843466:web:a36686f7af9cc491b029f3',
+};
+
+firebase.initializeApp(config);
